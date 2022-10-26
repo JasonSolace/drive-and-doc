93b17e024cc7d7c353ac4ca960de986118b67437
AndyRiedel <das_stig@yahoo.com>
2022-10-25 21:38:11 -0500

    testing

---
+++ api/trips/index.php
@@ -0,0 +1,33 @@
+<?php
+
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods: POST, PUT, DELETE, GET');
+    header('Access-Control-Allow-Headers: Access-Control-Allow-Origin,Content-Type, Access-Control-Allow-Methods,Authorization, X-Requested-With');
+
+
+    $request_method = $_SERVER['REQUEST_METHOD'];
+
+    switch ($request_method){
+        case 'GET':
+           
+            require 'request.php';
+            break;
+        case 'POST':
+            require 'create.php';
+            break;
+        case 'PUT':
+            require 'update.php';
+            break;
+        case 'DELETE':
+            require 'delete.php';
+            break;
+
+    }
+
+
+
+
+
+
+?>
