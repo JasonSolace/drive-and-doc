3dfd3f879bf9d47eb790d78e4373cadb90655192
AndyRiedel <das_stig@yahoo.com>
2022-10-26 23:18:23 -0500

    added trip update

--- models/Trip.php
+++ models/Trip.php
@@ -115,6 +115,32 @@ class Trip {
 
     }
 
+    public function tripCheck(){
+        //checks to see if the user trip exists in the trip table
+        //returns boolean
+
+        $query = 'SELECT COUNT(*) "tripIdCount" FROM TRIP WHERE ID = :id';
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':id', $this->id, PDO_PARAM_INT);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($stmt->execute()){
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            extract($row);
+            if ($tripIdCount > 0){
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            printf('ERROR: %s.\n', $stmt->error);
+            return false;
+        }
+
+}
+
+
     public function create(){
         //creates a new trip in the database
         $query = 'INSERT INTO TRIP (
@@ -251,6 +277,127 @@ class Trip {
         else {
             printf('ERROR: %s.\n', $stmt->error);
         }
+    }
+
+
+    public function update(){
+        //updates a trip record
+        $query = 'UPDATE TRIP 
+                  SET '
+                  . (is_null($this->tripStatus) ? '' : 'TripStatus = :tripStatus,') 
+                  . (is_null($this->startDateTime) ? '' : 'StartDateTime = :startDateTime,')
+                  . (is_null($this->endDateTime) ? '' : 'EndDateTime  = :startDateTime,')
+                  . (is_null($this->startCity) ? '' : 'StartCity  = :startDateTime,')
+                  . (is_null($this->startStateCode) ? '' : 'StartStateCode  = :startDateTime,')
+                  . (is_null($this->endCity) ? '' : 'EndCity = :startDateTime,')
+                  . (is_null($this->endStateCode) ? '' : 'EndStateCode  = :startDateTime,')
+                  . (is_null($this->userId) ? '' : 'UserId  = :startDateTime,')
+                  . (is_null($this->loadContents) ? '' : 'LoadContents  = :startDateTime,')
+                  . (is_null($this->loadWeight) ? '' : 'LoadWeight  = :startDateTime,');
+        $query = rtrim($query, ',') . 'WHERE ID = :id'; //remove the final comma and add where clause
+
+        //create a sql statement
+        $stmt = $this->conn->prepare($query);
+
+        //clean to prevent sql injection
+        $this->tripStatus = htmlspecialchars(strip_tags($this->tripStatus));
+        $this->startDateTime = htmlspecialchars(strip_tags($this->startDateTime));
+        $this->endDateTime = htmlspecialchars(strip_tags($this->endDateTime));
+        $this->startCity = htmlspecialchars(strip_tags($this->startCity));
+        $this->startStateCode = htmlspecialchars(strip_tags($this->startStateCode));
+        $this->endCity = htmlspecialchars(strip_tags($this->endCity));
+        $this->endStateCode = htmlspecialchars(strip_tags($this->endStateCode));
+        $this->userId = htmlspecialchars(strip_tags($this->userId));
+        $this->loadContents = htmlspecialchars(strip_tags($this->loadContents));
+        $this->loadWeight = htmlspecialchars(strip_tags($this->loadWeight));
+        
+        //bind data, but only if field is set so we don't overwrite with null
+        if (!is_null($this->tripStatus)){
+            $stmt->bindParam(':tripStatus', $this->tripStatus); 
+        }
+        if (!is_null($this->startDateTime)){
+            $stmt->bindParam(':startDateTime', $this->startDateTime);
+        }
+        if (!is_null($this->endDateTime)){ 
+            $stmt->bindParam(':endDateTime', $this->endDateTime); 
+        }
+        if (!is_null($this->startCity)){
+            $stmt->bindParam(':startCity', $this->startCity); 
+        }
+        if (!is_null($this->startStateCode)){
+            $stmt->bindParam(':startStateCode', $this->startStateCode); 
+        }
+        if (!is_null($this->endCity)){
+            $stmt->bindParam(':endCity', $this->endCity); 
+        }
+        if (!is_null($this->endStateCode)){
+            $stmt->bindParam(':endStateCode', $this->endStateCode); 
+        }
+        if (!is_null($this->userId)){
+            $stmt->bindParam(':userId', $this->userId, PDO::PARAM_INT); 
+        }
+        if (!is_null($this->loadContents)){
+            $stmt->bindParam(':loadContents', $this->loadContents); 
+        }
+        if (!is_null($this->loadWeight)){
+            $stmt->bindParam(':loadWeight', $this->loadWeight, PDO::PARAM_INT); 
+        }
+
+        //update the record
+        if ($stmt->execute()){
+            //get the updated record
+            $newTripId = '
+                SELECT 
+                    t.ID,
+                    t.TripStatus,
+                    t.StartDateTime,
+                    t.EndDateTime,
+                    t.StartCity,
+                    t.StartStateCode,
+                    t.EndCity,
+                    t.EndStateCode,
+                    t.UserId,
+                    u.FirstName,
+                    u.LastName,
+                    t.LoadContents,
+                    t.LoadWeight
+                FROM TRIP t
+                    INNER JOIN USER u
+                        ON u.ID = t.userId
+                WHERE t.ID = :id';
+            $stmtId = $this->conn->prepare($newTripId);
+            $stmtId->bindParam(':id', $this->id, PDO::PARAM_INT);
+            if ($stmtId->execute()){
+                $row = $stmtId->fetch(PDO::FETCH_ASSOC);
+                extract($row);
+                $this->id = $ID;
+                $this->tripStatus = $TripStatus;
+                $this->startDateTime = $StartDateTime;
+                $this->endDateTime = $EndDateTime;
+                $this->startCity = $StartCity;
+                $this->startStateCode = $StartStateCode;
+                $this->endCity = $EndCity;
+                $this->endStateCode = $EndStateCode;
+                $this->userId = $UserId;
+                $this->userFirstName = $FirstName;
+                $this->userLastName = $LastName;
+                $this->loadContents = $LoadContents;
+                $this->loadWeight = $LoadWeight;
+                
+            } else {
+                printf('Error retrieving updated Trip: %s.\n', $stmtId->error);
+            }
+            return true;
+
+        }
+        else {
+            //print error
+            printf('Error: %s.\n', $stmt->error);
+            return false;
+        }
+
+
+
     }
 
 }
