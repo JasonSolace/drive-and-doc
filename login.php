688afece7c5d8e2de7c05efa7bcc072e2b0edc70
mganthony-fhsu <mganthony@mail.fhsu.edu>
2022-10-30 17:14:46 -0400

    implemented login and logout

---
+++ login.php
@@ -0,0 +1,93 @@
+<?php
+// Initialize the session
+session_start();
+
+// Check if the user is already logged in, if yes then redirect him to welcome page
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: ./views/driver/home.php");
+    exit;
+}
+
+// Include Database config file
+require_once "./config/Database.php";
+include_once "./models/User.php";
+
+// Instantiate DB object
+$database = new Database();
+$db = $database->connect();
+
+// Instantiate User model
+$user_obj = new User($db);
+
+// Define variables and initialize with empty values
+$username_err = $password_err = $login_err = "";
+
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    // Check if username is empty
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter username.";
+    } else{
+        $user_obj->username = trim($_POST["username"]);
+    }
+    
+    // Check if password is empty
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter your password.";
+    } else{
+        $user_obj->password = trim($_POST["password"]);
+    }
+    
+    // Validate credentials
+    if(empty($username_err) && empty($password_err)){
+        if ($user_obj->loginIsValid()) {
+            header("location: index.php");
+        } else {
+            $login_err = "Invalid username or password.";
+        }
+    }
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 360px; padding: 20px; }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Login</h2>
+        <p>Please fill in your credentials to login.</p>
+
+        <?php 
+        if(!empty($login_err)){
+            echo '<div class="alert alert-danger">' . $login_err . '</div>';
+        }        
+        ?>
+
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group">
+                <label>Username</label>
+                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                <span class="invalid-feedback"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
+                <span class="invalid-feedback"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Login">
+            </div>
+            <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
+        </form>
+    </div>
+</body>
+</html>
--- models/User.php
+++ models/User.php
@@ -6,10 +6,13 @@ class User {
     private $table = 'USER';
     private $param_username;
     private $param_password;
+    private $hashed_password;
+
 
     //user properties
     public $id;
     public $username;
+    public $password;
     public $firstName;
     public $lastName;
 
@@ -23,7 +26,7 @@ class User {
     public function userExists() {
         $result = false;
         // Prepare a select statement
-        $sql = "SELECT id FROM ". $this->table ." WHERE username = :username";
+        $sql = "SELECT ID FROM ". $this->table ." WHERE Username = :username";
 
         if($stmt = $this->conn->prepare($sql)){
             // Bind variables to the prepared statement as parameters
@@ -46,7 +49,55 @@ class User {
         }
         return $result;
     }
+    
+    public function loginIsValid() {
+        // Prepare a select statement
+        $sql = "SELECT ID, Username, Password FROM ". $this->table ." WHERE Username = :username";
+        
+        if($stmt = $this->conn->prepare($sql)){
+            // Bind variables to the prepared statement as parameters
+            $stmt->bindParam(":username", $this->param_username, PDO::PARAM_STR);
+            
+            // Set parameters
+            $this->param_username = $this->username;
+            
+            // Attempt to execute the prepared statement
+            if($stmt->execute()){
+                // Check if username exists, if yes then verify password
+                if($stmt->rowCount() == 1){
+                    if($row = $stmt->fetch()){
+                        $this->id = $row["ID"];
+                        $this->username = $row["Username"];
+                        $this->hashed_password = $row["Password"];
+                        if(password_verify($this->password, $this->hashed_password)){
+                            // Password is correct, so start a new session
+                            session_start();
+                            
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $this->id;
+                            $_SESSION["username"] = $this->username;                            
+                            
+                            // Redirect user to welcome page
+                            return true;
+                        } else{
+                            // Password is not valid, display a generic error message
+                           return false;
+                        }
+                    }
+                } else{
+                    // Username doesn't exist, display a generic error message
+                    return false;
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
 
+            // Close statement
+            unset($stmt);
+        }
+
+    }
 
     public function create(){ 
         // Prepare an insert statement
