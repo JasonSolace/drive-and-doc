a518e3ee670c53449f7dd6f979b019202211dd6f
mganthony-fhsu <mganthony@mail.fhsu.edu>
2022-11-04 07:16:12 -0400

    removed html on index.php since it is just handling redirects now

--- index.php
+++ index.php
@@ -16,29 +16,3 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
     exit;
 }
 ?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Drive and Doc Login</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="./css/loginStylesheet.css">
-    </head>
-    <body>
-        <div class="container">
-            <form action = "views/admin/home.php" method="post">
-                <h1>Login</h1>                
-                <br>
-                <input type="text" placeholder="Enter Your Username" name="username" required>
-                <br>
-                <input type="text" placeholder="Enter Your Password" name="password" required>
-                <br>
-                <a href="" checked="checked">Forgot Password?</a>
-                <br>
-                <button type="submit" class="loginBtn">Login</button>
-            </form>
-            </div>
-    </body>
-</html>
