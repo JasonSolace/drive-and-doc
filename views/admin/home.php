3e08e3c4850c53cef6163838965eca73c4099aae
sisemorea <addysonsisemore@gmail.com>
2022-11-04 16:53:27 -0500

    Updated Redirection Links

--- views/admin/home.php
+++ views/admin/home.php
@@ -27,7 +27,7 @@
         #local
         #curl_setopt($ch, CURLOPT_URL, 'http://localhost/drive-and-doc/api/trips/?queryStr=' . $queryString);//define url as api target, must change to prod
         #prod
-        curl_setopt($ch, CURLOPT_URL, 'http://drive-and-doc.herokuapp.com/api/trips/?queryStr=' . $queryString);//define url as api target, must change to prod
+        curl_setopt($ch, CURLOPT_URL, 'http://drive-and-doc.herokuapp.com/controllers/api/trips/?queryStr=' . $queryString);//define url as api target, must change to prod
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
         $queryResult = curl_exec($ch);//send the curl request
         curl_close($ch);
@@ -42,7 +42,7 @@
     #local
     #curl_setopt($ch, CURLOPT_URL, 'http://localhost/drive-and-doc/api/trips/?userId=' . $userId);
     #prod
-    curl_setopt($ch, CURLOPT_URL, 'http://drive-and-doc.herokuapp.com/api/trips/?userId=' . $userId);
+    curl_setopt($ch, CURLOPT_URL, 'http://drive-and-doc.herokuapp.com/controllers/api/trips/?userId=' . $userId);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
     $result = curl_exec($ch); //send the curl request
     curl_close($ch);
