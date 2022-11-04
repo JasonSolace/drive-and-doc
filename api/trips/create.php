681a80bf1c2e64009381c35c9103ae26cbe33a80
addsisem <addysonsisemore@gmail.com>
2022-11-03 19:55:54 -0500

    Updated Create Trip to Upload Data to the DB

--- api/trips/create.php
+++ api/trips/create.php
@@ -14,24 +14,23 @@
        $trip = new Trip($db);
 
         //TRIP requires a startdatetime and driver user id
-       if (isset($_GET['driverUserId']) 
-            && $_GET['driverUserId'] != ''
-            && isset($_GET['startDatetime']) 
-            && DateTime::createFromFormat('Y-m-d H:i:s', str_replace(['%', 'T'], ' ', $_GET['startDatetime'])) != false) {
+       /*if (!empty($_POST['driverUserId'])
+            && !empty($_POST['startDatetime']) 
+            && DateTime::createFromFormat('Y-m-d H:i:s', str_replace(['%', 'T'], ' ', $_POST['startDatetime'])) != false) {*/
                 //if required params are passed, create a trip
 
-                $trip->userId = is_int($_GET['driverUserId']) ? $_GET['driverUserId'] : intval($_GET['driverUserId']);
+                $trip->userId = is_int($_POST['driverUserId']) ? $_POST['driverUserId'] : intval($_POST['driverUserId']);
                 
-                $trip->tripStatus = $_GET['tripStatus'];
-                $trip->startDateTime = $_GET['startDatetime'];
-                $trip->endDateTime = $_GET['endDatetime'];
-                $trip->startCity = $_GET['startCity'];
-                $trip->startStateCode = $_GET['startStateCode'];
-                $trip->endCity = $_GET['endCity'];
-                $trip->endStateCode = $_GET['endStateCode'];
-                $trip->loadContents = $_GET['loadContents'];
-                $trip->loadWeight = $_GET['loadWeight'];
-                $trip->companyId = $_GET['companyId'];
+                $trip->tripStatus = $_POST['tripStatus'];
+                $trip->startDateTime = $_POST['startDatetime'];
+                $trip->endDateTime = $_POST['endDatetime'];
+                $trip->startCity = $_POST['startCity'];
+                $trip->startStateCode = $_POST['startStateCode'];
+                $trip->endCity = $_POST['endCity'];
+                $trip->endStateCode = $_POST['endStateCode'];
+                $trip->loadContents = $_POST['loadContents'];
+                $trip->loadWeight = $_POST['loadWeight'];
+                $trip->companyId = $_POST['companyId'];
                 
                 //test if passed user id is in the db
                 if (!$trip->userCheck()){
@@ -42,35 +41,19 @@
                 //assume here passed user id is in the database, and passed date string is of valid format
                 else if ($trip->create()){
                     //if trip creation is successful, return the Trip
-                    echo json_encode(
-                        array(
-                            'ID' => $trip->id,
-                            'tripStatus' => $trip->tripStatus,
-                            'startDateTime' => $trip->startDateTime,
-                            'endDateTime' => $trip->endDateTime,
-                            'startCity' => $trip->startCity,
-                            'startStateCode' => $trip->startStateCode,
-                            'endCity' => $trip->endCity,
-                            'endStatecode' => $trip->endStateCode,
-                            'driverUserId' => $trip->userId,
-                            'driverFirstName' => $trip->userFirstName,
-                            'driverLastName' => $trip->userLastName,
-                            'loadContents' => $trip->loadContents,
-                            'loadWeight' => $trip->loadWeight,
-                            'companyId' => $trip->companyId
-                        )
-                        );
+                    header("Location: ../../views/admin/trip_detail.php");
+                    exit;
                 }
                 else {
                     echo json_encode(
                         array('message' => 'Trip not created')
                     );
                 }
-        }
+        /*}
         else {
             print_r(json_encode(array('message' => 'Must pass driverUserId and startDatetime to create a trip!')));
             die(); //exit
-        }
+        }*/
 
 
 ?>
